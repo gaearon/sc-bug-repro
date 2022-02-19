901332507a0b842386d3ceeadc66185f19a7ab7f
Dan Abramov <dan.abramov@gmail.com>
2022-02-19 22:38:50 +0000

    add log

--- pages/index.server.js
+++ pages/index.server.js
@@ -1,6 +1,7 @@
 import styles from '../styles/Home.module.css'
 
 export default function Home({ router }) {
+  console.log('render. router is ', router)
   return (
     <h1>Hi {router.pathname}</h1>
   )
