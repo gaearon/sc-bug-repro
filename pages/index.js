ba248125cacb10f0ed3d1c83468c20fc32afda38
Dan Abramov <dan.abramov@gmail.com>
2022-05-23 21:50:52 +0100

    render twice repro

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,8 @@
 import * as React from 'react';
 
-let Something = require('../components/Something').default
-if (typeof window !== 'undefined') {
-  Something = React.lazy(() => import('../components/Something').then(e =>
-    new Promise(resolve => setTimeout(() => resolve(e), 2000))
-  ));
-}
-
 export default function MyPage() {
-  return (
-    <React.Suspense fallback={<h2>oh no...</h2>}>
-      <Something />
-    </React.Suspense>
-  )
+  React.useEffect(() => {
+    console.log('effect')
+  })
+  return <h1>Hello</h1>
 }
